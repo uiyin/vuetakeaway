95adcff426ba049542595babd1e2050cac432a32
triggers <triggersshow@gmail.com>
2018-11-18 08:55:13 +0800

    完成初始配置结构

--- src/main.js
+++ src/main.js
@@ -3,13 +3,19 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-
+import FastClick from 'fastclick'
+import '@/assets/styles/css/reset.css'
+import '@/assets/styles/css/border.css'
+import '@/assets/styles/css/iconfont.css'
+FastClick.attach(document.body)
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
-  components: { App },
+  components: {
+    App
+  },
   template: '<App/>'
 })
---
+++ src/pages/Index/Index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <div class="iconfont icon-zan"
+         style="width:100px;height:100px;color:black;"></div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      message: '首页'
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
